3def9baa0cb0a7898a46d841b79bfddd7ab33929
a.stockmayer@googlemail.com <a.stockmayer@googlemail.com>
2024-01-16 08:38:07 +0000

    Created Home; Edited settings and Form1

--- client_code/Home/__init__.py
+++ client_code/Home/__init__.py
@@ -1,18 +1,16 @@
-from ._anvil_designer import Form1Template
+from ._anvil_designer import HomeTemplate
 from anvil import *
 import anvil.google.auth, anvil.google.drive
 from anvil.google.drive import app_files
+import anvil.users
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
-import anvil.users
 
-class Form1(Form1Template):
+class Home(HomeTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-    anvil.users.login_with_form()
+
     # Any code you write here will run before the form opens.
-    self.navigation_bar.add_component(Button(text="foo"))
-    self.content_panel.clear()
-    self.content_panel.add_component(Home())
+    self.label_user.text = anvil.users.get_user()
