f4ba664aae775c68dfe9459ca0e38bfc94dbfd31
a.stockmayer@googlemail.com <a.stockmayer@googlemail.com>
2024-01-22 15:08:40 +0000

    Edited Project

--- client_code/Project/__init__.py
+++ client_code/Project/__init__.py
@@ -49,9 +49,11 @@ class Project(ProjectTemplate):
   def view_button_clicked(self, **event_args):
     target = event_args['sender'].tag.target
     self.content_panel.clear()
-    panel_tmp = XYPanel(width="100%",height="100%")
+    panel_tmp = XYPanel(width="1000px",height="1000px")
     self.content_panel.add_component(panel_tmp)
-    code = "<div id='web_forward_external'></div> <script type='text/javascript'> $(document).ready(function (){$('#web_forward_external').load(%s);});</script>"%(target)
+    #code = "<div id='web_forward_external'></div> <script type='text/javascript'> $(document).ready(function (){$('#web_forward_external').load(%s);});</script>"%(target)
+    #code = "<object data=\"https://10.0.0.1\" width=\"400\" height=\"300\" type=\"text/html\"> Alternative Content </object>"
+    code = "<iframe src=\"https://10.0.0.1\" style=\"border:0px #ffffff none;\" name=\"myiFrame\" scrolling=\"no\" frameborder=\"1\" marginheight=\"0px\" marginwidth=\"0px\" height=\"400px\" width=\"600px\" allowfullscreen></iframe>"
     print(code)
     iframe = jQuery(code)
     iframe.appendTo(get_dom_node(panel_tmp))
